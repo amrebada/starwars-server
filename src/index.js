54de3fd21a3a26fe590868954380d790848bd8ef
amrebada <amr.app.engine@gmail.com>
2020-01-04 03:01:53 +0400

    cors added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import graphQlHTTP from "express-graphql";
 import bodyParser from "body-parser";
+import cors from "cors";
 import schema from "./schemas";
 import dbInit from "./models/index";
 import config from "./config";
@@ -8,6 +9,11 @@ import auth from "./routes/auth";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*"
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
